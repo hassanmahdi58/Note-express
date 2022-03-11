60c51485d02ac716a6189dd8a2991f6204f3431e
hassanmahdi58 <hassanmahdi58@gmail.com>
2022-03-11 12:05:25 +0000

    make it better

--- server.js
+++ server.js
@@ -1,60 +1,79 @@
-//const PORT = process.env.PORT || 3000;
-const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser')
+var express = require("express");
+var path = require("path");
+var fs = require("fs").promises;
+var notes = require("./db/db.json")
 
-let note = [{ id: 1, body: 'We have a text' }, { id: 2, body: 'This is a second text' }];
+var app = express();
 
-const allNotes = require('./develop/db/db.json');
-
-
-const express = require('express');
-
-const app = express();
+var PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+
+allnote = notes.length;
 
 
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
+app.get("/api/notes", function (req, res) {
+
+    return res.json(notes);
+});
+app.use(express.static("public"));
+
+
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
+// Post function
+app.post("/api/notes", function (req, res) {
+    var newPost = req.body;
+
+    newPost["id"] = allnote +1;
+    allnote++;
+    console.log(newPost);
+
+    notes.push(newPost);
+
+    keepnotedb();
+
+    return res.status(200).end();
 });
+ 
+// Delete function
+app.delete("/api/notes/:id", function (req, res) {
+    res.send('DELETE request at /api/notes/:id')
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/index.html'));
-});
+    var id = req.params.id;
+
+    var idu = notes.filter(function (u) {
+        return u.id < id;
+    });
+
+    var idp = notes.filter(function (p) {
+        return p.id > id;
+    });
+
+    notes = idu.concat(idp);
+
+  keepnotedb();
+})
 
 
-//then, we use app.post option.
-app.post("/Notes", function (req, res) {
-  //assigning Note id to the notes using math.random
-  const userNote = {};
-  userNotes.id = Math.random() * 100;
-  userNotes.body = req.body.newNote
-  note.push(userNotes);
-  //then we redirect it to the root route
-  res.redirect('./develop/db/db.json');
-});
+ 
 
-//Handling the delete request
+function keepnotedb(){
+fs.writeFile ("db/db.json", JSON.stringify(notes), function(err) {
+    if (err) throw err;
+    console.log('complete');
+    });
+}
 
-app.post('/deleteNote/:id', function (req, res) {
-  console.log(req.params.id);
-  const deleteNotes = note.filter(item => item.id != req.params.id);
-  allNotes = deleteNotes;
-  return res.redirect('./develop/db/db.json');
-});
-
-//then we set our server port. This should always be at bottom.
-app.listen(5000, function () {
-  console.log("NoteApp server is running at port 5000")
-});
+app.listen(PORT, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
+})
